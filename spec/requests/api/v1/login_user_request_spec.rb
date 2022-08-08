dd66ab85c8c517d970c419dff4dfccc43634edf8
Ian Ross <ianrossdrums@gmail.com>
2022-08-08 17:28:35 -0600

    test: sad path for login

--- spec/requests/api/v1/login_user_request_spec.rb
+++ spec/requests/api/v1/login_user_request_spec.rb
@@ -23,4 +23,34 @@ RSpec.describe "login endpoint" do
     expect(data[:attributes][:email]).to eq(user.email)
     expect(data[:attributes][:api_key]).to eq(user.api_key)
   end
+
+  describe 'sad path' do
+    before :each do
+      User.create(email: "pabu@example.com", password: "pabu123", password_confirmation: "pabu123", api_key: "abcde12345")
+    end
+    it "has wrong email" do
+      params = {
+        email: "loki@example.com",
+        password: "pabu123"
+      }
+      headers = { "Content-Type" => "application/json" }
+
+      post "/api/v1/sessions", headers: headers, params: JSON.generate(params)
+
+      expect(response).to_not be_successful
+      expect(response.body).to eq("Invalid info")
+    end
+    it "has wrong email" do
+      params = {
+        email: "pabu@example.com",
+        password: "pabu"
+      }
+      headers = { "Content-Type" => "application/json" }
+
+      post "/api/v1/sessions", headers: headers, params: JSON.generate(params)
+      
+      expect(response).to_not be_successful
+      expect(response.body).to eq("Invalid info")
+    end
+  end
 end
