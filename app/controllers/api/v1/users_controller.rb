769b48f19e54beb288b41cdc95de3350d401b85d
Ian Ross <ianrossdrums@gmail.com>
2022-08-08 16:37:05 -0600

    feat: user creation route, controller, and model

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,10 +1,10 @@
 class Api::V1::UsersController < ApplicationController
-  def new
+  def create
     user = User.new(user_params)
     if user.save
-      render json: UserSerializer.new(user), status: 201
+      render json: UserSerializer.create_user(user), status: 201
     else
-      render status: 400
+      render json: user.errors.full_messages.to_sentence.to_s, status: 400
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  validates_presence_of :email, :password, :password_confirmation
   validates_uniqueness_of :email
   has_secure_password
 end
