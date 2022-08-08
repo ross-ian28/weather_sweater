f3caff68922a1b546d1d117539d435f97ac3abac
Ian Ross <ianrossdrums@gmail.com>
2022-08-08 10:52:47 -0600

    test: more robust request testing

--- spec/requests/munchies_request_spec.rb
+++ spec/requests/munchies_request_spec.rb
@@ -11,8 +11,16 @@ RSpec.describe "munchies api" do
     expect(data).to include :id, :type, :attributes
     expect(data[:id]).to eq(nil)
     expect(data[:type]).to eq("munchie")
+
     expect(data[:attributes]).to include :destination_city, :forecast, :restaurant
+    expect(data[:attributes][:destination_city]).to eq("denver,co")
+
     expect(data[:attributes][:forecast]).to include :summary, :temperature
+    expect(data[:attributes][:forecast][:summary]).to be_a(String)
+    expect(data[:attributes][:forecast][:temperature]).to be_an(Float)
+
     expect(data[:attributes][:restaurant]).to include :name, :address
+    expect(data[:attributes][:restaurant][:name]).to be_a(String)
+    expect(data[:attributes][:restaurant][:address]).to be_a(String)
   end
 end
