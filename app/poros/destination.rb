7f608777e6dabb85a6031b8cfc13378da2e7f389
Ian Ross <ianrossdrums@gmail.com>
2022-08-09 15:07:45 -0600

    feat: forcast and destination poros

--- app/poros/destination.rb
+++ app/poros/destination.rb
@@ -1,7 +1,13 @@
 class Destination
-  attr_reader :travel_time
+  attr_reader :travel_time, :hour, :error, :seconds
 
   def initialize(data)
-    @travel_time = "#{data[:route][:realTime] / 3600} hours, #{(data[:route][:realTime] % 3600) / 60} minutes"
+    if data[:info][:messages] == ["We are unable to route with the given locations."]
+      @error = "error"
+    else
+      @seconds = data[:route][:legs][0][:time]
+      @hour = data[:route][:legs][0][:time] / 3600
+      @travel_time = "#{data[:route][:legs][0][:time] / 3600} hours, #{(data[:route][:legs][0][:time] % 3600) / 60} minutes"
+    end
   end
 end
