28dbfa8afe6232b585a6b396471f99bd9cb10e7f
Ian Ross <ianrossdrums@gmail.com>
2022-08-09 15:08:35 -0600

    feat: road trip controller

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,10 +1,19 @@
 class Api::V1::RoadTripController < ApplicationController
   def index
-    if params[:api_key] != nil
+    if User.find_by(api_key: params[:api_key])
       trip = RoadTripFacade.trip(params[:origin], params[:destination])
-      RoadTripSerializer.get_trip(params[:origin], params[:destination], trip)
+      if trip.error != nil || trip.seconds >= 10000000
+        render json: RoadTripSerializer.bad_trip(params[:origin], params[:destination])
+      elsif (trip.hour / 24) > 8
+        render json: "Too far in advance to determine"
+      else
+        cords = GeocodeFacade.cords(params[:location])
+        weather = WeatherFacade.future_weather(cords[0].lat, cords[0].lng, trip.hour)
+
+        render json: RoadTripSerializer.get_trip(params[:origin], params[:destination], trip, weather)
+      end
     else
-      render json: 'Api key required', status: 401
+      render json: 'Unauthorized', status: 401
     end
   end
 end
